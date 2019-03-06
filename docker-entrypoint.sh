d248702fbaeea4c5a20980f580e40a1404752915
blademainer <blademainer@gmail.com>
2019-03-07 00:49:49 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DEFAULT_TZ="Asia/Shanghai"
+if [ -z "$TZ" ]; then
+ export TZ="$DEFAULT_TZ"
+fi
+echo "TZ: $TZ"
+
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+npm start
+
